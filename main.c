815eda947ab6227c22ca79b927837a7ce4fe32d2
MazvitaMavhaire <mazvita2.mavhaire@live.uwe.ac.uk>
2026-04-28 18:26:26 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,50 @@
-#include <stdio.h>
+include <stdio.h> // allows printing
+#include <stdlib.h>
+#include <math.h>
+
+#ifndef WAVEFORM_H
+#define WAVEFORM_H
+
+typedef struct{
+    double timestamp;
+    double phaseA;
+    double phaseB;
+    double phaseC;
+    double current;
+    double frequency;
+    double powerFactor;
+    double thd;
+} WaveformSample;
 
 int main() {
-    printf("My name is!\n");
-    return 0;
+printf("Power Analyer Started\n");
+
+FILE*file;
+WaveformSample*samples;
+file= fopen("power_quality_log.csv", "r");
+
+if (file==NULL){
+printf("Error opening file.\n");
+return 1;
 }
+samples=malloc(1000*sizeof(WaveformSample));
+
+fscanf(file, "%&*[
+
+while (fscanf(file, "%lf,%lf,%lf,%lf,%lf,%lf,%lf,%lf"
+                  &samples[count].timestamp,
+                  &samples[count].phaseA,
+                  &samples[count].phaseB,
+                  &samples[count].phaseC,
+                  &samples[count].current,
+                  &samples[count].frequency,
+                  &samples[count].powerFactor,
+                  &samples[count].thd) == 8) {
+        count++;
+    }
+free(samples);
+    fclose(file);
+return0;
+}
+
+   
