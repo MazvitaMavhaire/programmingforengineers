9d7dcbade5296033c64dc876706cbe52e0462ded
mv2-mavhaire <mazvita2.mavhaire@live.uwe.ac.uk>
2026-04-30 18:06:37 +0100

    coursework

--- waveform.h
+++ waveform.h
@@ -5,6 +5,13 @@
 
 #ifndef WAVEFORM_H
 #define WAVEFORM_H
+/*
+ * waveform.h
+ */
+#include <stddef.h>// gives us size_t
+#include <stdint.h> //gives us uint8_t
+
+#define NOMINAL_RMS_VOLTAGE 230.0// expected RMS voltage for each phase
 
 typedef struct{
     double timestamp;
